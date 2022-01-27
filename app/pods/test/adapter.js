0a91bd5d488f21488012698d930c53a2bab24ee5
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-01-27 09:58:31 -0500

    fix things

--- app/pods/test/adapter.js
+++
@@ -1,3 +0,0 @@
-import LSAdapter from 'ember-local-storage/adapters/local';
-export default class Adapter extends LSAdapter {}
-
--- app/routes/test/test.js
+++ app/routes/test/test.js
@@ -1,14 +1,18 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class TestRoute extends Route {
-  model() {
+  @service store;
+
+  model({ test_id }) {
     super.model(...arguments);
-    console.log('/test/test model');
-    const model = this.modelFor('application');
-    const { test_id } = this.paramsFor('test.test');
-    model.currentTestId = test_id;
+
+    let model = this.store.peekRecord('test', test_id);
+    console.log('/test/test model', model.id);
+
     return model;
   }
+
   afterModel() {
     super.afterModel(...arguments);
     console.log('/test/test afterModel');
